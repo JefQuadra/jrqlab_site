4c96a0337a5760f61559ee86faad33dd1bfefbb8
Mariana Almeida de Azevedo <jefquadra@gmail.com>
2023-03-05 21:07:33 -0300

    Working with JS.

--- index.js
+++ index.js
@@ -8,7 +8,43 @@ function myFunction() {
   document.getElementById("options-show").className =
     "navbar-collapse collapse";
 
-  if (document.body.scrollTop > 0 || document.documentElement.scrollTop > 0) {
-    console.log(document.getElementById("nav").scrollTop);
-  }
+  //if (document.body.scrollTop > 0 || document.documentElement.scrollTop > 0) {
+  //  console.log(document.getElementById("nav").scrollTop);
+  //}
 }
+
+const showMenu = document.querySelector("[show-menu]");
+showMenu.onclick = function (e) {
+  if (document.getElementById("menu-options").className == "show-div") {
+    let fontOpt = document.getElementsByClassName("menu-option");
+    let initialSize = 16;
+    id1 = setInterval(decrease, 15);
+    function decrease() {
+      if (initialSize == 0) {
+        clearInterval(id1);
+        document.getElementById("menu-options").className = "hidden-div";
+      } else {
+        for (let e of fontOpt) {
+          e.style.fontSize = initialSize + "px";
+        }
+        initialSize = initialSize - 1;
+      }
+    }
+  } else {
+    document.getElementById("menu-options").className = "show-div";
+    let fontOpt = document.getElementsByClassName("menu-option");
+    let initialSize = 0;
+    id2 = setInterval(grow, 15);
+    function grow() {
+      if (initialSize == 16) {
+        clearInterval(id2);
+      } else {
+        for (let e of fontOpt) {
+          e.style.fontSize = initialSize + "px";
+        }
+        initialSize = initialSize + 1;
+      }
+    }
+  }
+  //console.log(document.getElementsByClassName("menu-options").className);
+};
